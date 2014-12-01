6212745baef9c361ca65e8ee873f14e96a012055
Alex Hawkins <alexhawkins.me@gmail.com>
2014-12-01 13:40:08 -0800

    Add database of supplements, add more later

--- app/js/app.js
+++ app/js/app.js
@@ -9,4 +9,61 @@ angular.module('supplementDiary', [])
     this.categories = categories;
     this.displayForm = false;
   })
+
+
+
+/*FAKE DATABASE*/
+
+var categories = ['fitness', 'sleep-aids', 'nootropic', 'memory', 'focus', 'detoxification', 'herbal', 'mood', 'energy', 'antioxidants', 'adaptogens', 'hormones', 'stress'];
+
+var supplements = [{
+  name: 'L-Theanine',
+  brand: 'Now Foods',
+  dose: '200mg',
+  asin: 'B000H7P9M0',
+  review: 'I am very skeptical when it comes to homeopathy and supplements. Much, if not most, is purely hype, misinformation, and lies. In this case, this is the real deal. Theanine, is very useful for anyone with issues ranging from mild to annoying anxieties to sleep issues to stress during the day. This really does the job at "taking the cares away," but not like Valium does, where you can zombie out. The best way to describe it is as a nice, calming feeling. You can still get all your work done, but not worry about things, which helps keep your focus together. When it comes to sleep, this is a good way to lull you to slumber by much the same way, removing any anxieties or bothers. Well worth the try, and I always trust products from Now.',
+  rating: 5,
+  categories: {
+    'sleep-aids': true,
+    focus: true,
+    stress: true
+  }
+}, {
+  name: 'L-Theanine',
+  brand: 'Now Foods',
+  dose: '200mg',
+  asin: 'B000H7P9M0',
+  review: 'I am very skeptical when it comes to homeopathy and supplements. Much, if not most, is purely hype, misinformation, and lies. In this case, this is the real deal. Theanine, is very useful for anyone with issues ranging from mild to annoying anxieties to sleep issues to stress during the day. This really does the job at "taking the cares away," but not like Valium does, where you can zombie out. The best way to describe it is as a nice, calming feeling. You can still get all your work done, but not worry about things, which helps keep your focus together. When it comes to sleep, this is a good way to lull you to slumber by much the same way, removing any anxieties or bothers. Well worth the try, and I always trust products from Now.',
+  rating: 5,
+  categories: {
+    'sleep-aids': true,
+    focus: true,
+    stress: true
+  }
+}, {
+  name: 'L-Theanine',
+  brand: 'Now Foods',
+  dose: '200mg',
+  asin: 'B000H7P9M0',
+  review: 'I am very skeptical when it comes to homeopathy and supplements. Much, if not most, is purely hype, misinformation, and lies. In this case, this is the real deal. Theanine, is very useful for anyone with issues ranging from mild to annoying anxieties to sleep issues to stress during the day. This really does the job at "taking the cares away," but not like Valium does, where you can zombie out. The best way to describe it is as a nice, calming feeling. You can still get all your work done, but not worry about things, which helps keep your focus together. When it comes to sleep, this is a good way to lull you to slumber by much the same way, removing any anxieties or bothers. Well worth the try, and I always trust products from Now.',
+  rating: 5,
+  categories: {
+    'sleep-aids': true,
+    focus: true,
+    stress: true
+  }
+}, {
+  name: 'L-Theanine',
+  brand: 'Now Foods',
+  dose: '200mg',
+  asin: 'B000H7P9M0',
+  review: 'I am very skeptical when it comes to homeopathy and supplements. Much, if not most, is purely hype, misinformation, and lies. In this case, this is the real deal. Theanine, is very useful for anyone with issues ranging from mild to annoying anxieties to sleep issues to stress during the day. This really does the job at "taking the cares away," but not like Valium does, where you can zombie out. The best way to describe it is as a nice, calming feeling. You can still get all your work done, but not worry about things, which helps keep your focus together. When it comes to sleep, this is a good way to lull you to slumber by much the same way, removing any anxieties or bothers. Well worth the try, and I always trust products from Now.',
+  rating: 5,
+  categories: {
+    'sleep-aids': true,
+    focus: true,
+    stress: true
+  }
+}];
+
 })();
