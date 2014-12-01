d29b6ae87122640c58dff033955b5ffeb95fee27
Alex Hawkins <alexhawkins.me@gmail.com>
2014-12-01 12:50:24 -0800

    implement angular module and SupplementDiaryController

--- app/js/app.js
+++ app/js/app.js
@@ -1,12 +1,12 @@
 'use strict';
 
 // Declare app level module which depends on views, and components
-angular.module('myApp', [
-  'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
-  'myApp.version'
-]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.otherwise({redirectTo: '/view1'});
-}]);
+
+(function() {
+angular.module('supplementDiary', [])
+  .controller('SupplementDiaryController', function() {
+    this.supplements = supplements;
+    this.categories = categories;
+    this.displayForm = false;
+  })
+})();
